c393fedae7b0a7f5b4200b6077274a2fc84380a2
Sumedha <sumedha.saxena@gmail.com>
2025-07-11 19:06:28 +0800

    update config

--- config.py
+++ config.py
@@ -3,16 +3,26 @@ import os
 MATCHMINER_SERVER = ""
 TOKEN = ""
 
-# Set BASE_DIRCTORY to the folder path where patient and trial data JSON files will be present
-# This should be the absolute path to the folder containing the PATIENT_FOLDER and TRIAL_JSON_FOLDER folders.
-BASE_DIR = ""
+# File watcher interval in minutes for checking new files
+WATCHER_INTERVAL_MINUTES = 120
 
-TRIAL_ENV_CONFIG_PATH = "matchminer_trial_data_env_config.json"
-TRIAL_JSON_FOLDER = os.path.join(BASE_DIR, "trial_data")
-TRIAL_JSON_PROCESSED_FOLDER = os.path.join(BASE_DIR, "trial_data_processed")
-PATIENT_FOLDER = os.path.join(BASE_DIR, "patient_data")
+# Set PATIENT_DATA_BASE_DIR to the folder path where patient's (clinical and genomic) JSON files will be present.
+# Ideally this would be the root directory of MATCHMINER_PATIENT repository.
+PATIENT_DATA_BASE_DIR = r"path\to\matchminer-patient"
+PATIENT_FOLDER = os.path.join(PATIENT_DATA_BASE_DIR, "patient_data_reviewed")
 PATIENT_CLINICAL_JSON_FOLDER = "clinical"
 PATIENT_GENOMIC_JSON_FOLDER = "genomic"
+
+# Set TRIAL_DATA_BASE_DIR to the folder path where trial JSON files will be present.
+# Ideally this would be the root directory of NCT2CTML repository.
+TRIAL_DATA_BASE_DIR = r"path\to\nct2ctml"
+TRIAL_FOLDER = os.path.join(TRIAL_DATA_BASE_DIR, "trial_data_reviewed")
+
+TRIAL_ENV_CONFIG_PATH = "matchminer_trial_data_env_config.json"
+
+TRIAL_JSON_PROCESSED_FOLDER = "trial_data_processed"
+PATIENT_JSON_PROCESSED_FOLDER = "patient_data_processed"
+
 TRIAL_ENDPOINT = "/api/trial"
 CLINICAL_ENDPOINT = "/api/clinical"
 GENOMIC_ENDPOINT = "/api/genomic"

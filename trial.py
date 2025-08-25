c4608b559f0eafb43f892e2f5224f64246e64430
Sumedha <sumedha.saxena@gmail.com>
2025-08-25 20:26:21 +0800

    update workflow to process trials based on last run date and trials from trial_status.csv (from nct2ctml repo)

--- trial.py
+++ trial.py
@@ -34,6 +34,7 @@ from loguru import logger
 import config
 import argparse
 import system
+import csv
 
 def load_environment_variables():
     """
@@ -67,6 +68,31 @@ def post_trial(trial_data):
         headers['Content-Type'] = f"application/json"
 
         response = requests.post(endpoint_url, json=trial_data, headers=headers, verify=False)
+        response.raise_for_status()
+        return response
+    except requests.exceptions.HTTPError as err:
+        print(f"HTTP error occurred: {err}, {err.response.content}")  # Handle HTTP errors
+    except Exception as err:
+        print(f"Other error occurred: {err}")  # Handle other exceptions
+
+def put_trial(matchminer_id,trial_data,etag):
+    """
+    Sends an API request to update trial data in matchminer system.
+    
+    Parameters:
+    trial_data (dict) : Trial Data
+    """
+    try:
+        endpoint_url = f'{urllib.parse.urljoin(f"{config.MATCHMINER_SERVER}", config.TRIAL_ENDPOINT,matchminer_id)}'
+        print(f"Posting request to {endpoint_url}")
+
+        headers = {
+            'Authorization': f"Basic {config.TOKEN}",
+            'Content-Type': "application/json",
+            'If-Match': etag 
+        }
+
+        response = requests.put(endpoint_url, json=trial_data, headers=headers, verify=False)
         response.raise_for_status()
         return response
     except requests.exceptions.HTTPError as err:
@@ -231,7 +257,7 @@ def main():
     args = parser.parse_args()
 
     if args.command == "insert":
-        insert_trials()
+        process_trials()
     elif args.command == "get":
         trial = get_trial_by_protocol_no(args.protocol_no)
         print(json.dumps(trial, indent=2) if trial else "No trial found.")
@@ -249,55 +275,190 @@ def main():
         save_to_file(all_nct_ids, "all_nct_ids", 'json')
         print("NCT Ids for all trials saved at all_nct_ids.json")
 
-def insert_trials():
+def process_trials():
     """
-    Code to insert trials into matchminer system.
+    Code to process trials. 
     It looks for matchminer-compliant trial JSON files at a specified path, and for each trial:
-     - calls a method to increment trial env variables
-     - calls a method to attach the updated env variables into the trial
      - calls a method to insert trial into matchminer system
-     - if successful, calls a method to save incremented env variables
-     - moves processed file to trial_data_processed folder
+     - calls a method to update trial in matchminer system
+     - calls a method to close trial in matchminer system
+     - calls a method to run matchengine
     """
     # Check if trial folder exists
     if not os.path.exists(config.TRIAL_DIR):
         logger.error(f"Trial folder does not exist: {config.TRIAL_DIR}")
         return False
     
+    # Check if trial_status.csv exists
+    if not os.path.exists(config.TRIAL_STATUS_CSV_PATH):
+        logger.error(f"Trial status csv not found: {config.TRIAL_STATUS_CSV_PATH}")
+        return False
+    
     processed_folder = config.TRIAL_JSON_PROCESSED_DIR
     if not os.path.exists(processed_folder):
         os.makedirs(processed_folder, exist_ok=True)
 
-    files = os.listdir(config.TRIAL_DIR)
-    any_success = False
-    for file in files:
-        full_path = os.path.join(config.TRIAL_DIR, file)
-        if os.path.isfile(full_path) and file.endswith(".json"):
+    # read trial_status.csv and filter trials to process based on last_run
+    trials_to_process = []
+    logger.info(f"Preparing a list of trials to process which were updated after last_run: {config.LAST_RUN}")
+    with open(config.TRIAL_STATUS_CSV_PATH, 'r', encoding='utf-8') as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            if row['entry_last_updated_date'] > config.LAST_RUN:
+                trials_to_process.append(row)
+
+    trials_to_update = [] # stores filename, matchminer id, protocol_id, protocol_no of trials to update
+    trials_to_insert = [] # stores filename which would contain trial data to be inserted
+    trials_to_close = [] # stores matchminer id, trial data of trials to close
+    for trial_to_process in trials_to_process:
+        nct_id = trial_to_process['nct_id']
+        file_name = _get_trial_file_name(trial_to_process)
+        if nct_id and nct_id != 'NA':
+            trial_in_mm = get_trial_by_nct_id(nct_id)
+            if trial_in_mm:
+                if trial_to_process['status'] == 'closed' and trial_in_mm['status'] != 'closed':
+                    trials_to_close.append((trial_in_mm['_id'], trial_in_mm))
+                else:
+                    trials_to_update.append((file_name, trial_in_mm['_id'], trial_in_mm['protocol_id'], trial_in_mm['protocol_no'], trial_in_mm['_etag']))
+            else:                
+                trials_to_insert.append(file_name)
+        else:
+            local_protocol_ids_string = trial_to_process['local_protocol_ids']
+            if local_protocol_ids_string and local_protocol_ids_string != 'NA':
+                local_protocol_ids = local_protocol_ids_string.split('|')
+                trial_in_mm = get_trial_by_local_protocol_ids(local_protocol_ids)
+                if trial_in_mm:
+                    trials_to_update.append((file_name,trial_in_mm['_id'], trial_in_mm['protocol_id'], trial_in_mm['protocol_no'], trial_in_mm['_etag']))
+                else:
+                    trials_to_insert.append(file_name)
+
+    logger.info(f"Trials to insert: {trials_to_insert}")
+    logger.info(f"Trials to update: {trials_to_update}")
+    logger.info(f"Trials to close: {[trial[0] for trial in trials_to_close]}")
+
+
+    # process new trials
+    any_success = _process_trials_to_insert(trials_to_insert, any_success)
+
+    # process trials to update
+    any_success = _process_trials_to_update(trials_to_update, any_success)
+
+    # process trials to close
+    any_success = _process_trials_to_close(trials_to_close, any_success)
+           
+    # Call run_matchengine once after all files processed, if any were successful
+    if any_success:
+        system.run_matchengine()
+    any_success = True
+    return any_success
+
+
+def _process_trials_to_insert(trials_to_insert, any_success):
+    """
+    Process trials that need to be inserted.
+    
+    Parameters:
+    trials_to_insert (list): List of file names to insert
+    any_success (bool): Current success status
+    
+    Returns:
+    bool: Updated success status
+    """
+    for trial_to_insert in trials_to_insert:
+        file_name = trial_to_insert
+        full_path = os.path.join(config.TRIAL_DIR, file_name)
+        if os.path.isfile(full_path):            
             try:
                 with open(full_path, 'r', encoding='utf-8') as json_file:
                     data = json.load(json_file)
 
-                env_variables = load_environment_variables()
-                
+                env_variables = load_environment_variables()                    
                 updated_env_variables = update_env_variables(env_variables)
-
                 updated_data = pre_process_trial_data(data, updated_env_variables)
                 response = post_trial(updated_data)
                 if response and response.status_code >= 200 and response.status_code < 300:
-                    logger.info(f"Successfully inserted {file}")
+                    logger.info(f"Successfully inserted {file_name}")
                     save_environment_variables(updated_env_variables)
                     # Move processed file to processed_folder with retry logic
-                    dest_path = os.path.join(processed_folder, file)
-                    _move_file_with_retry(full_path, dest_path)
+                    # dest_path = os.path.join(processed_folder, file_name)
+                    # _move_file_with_retry(full_path, dest_path)
                     any_success = True
                 else:
                     logger.error(f"Error while posting trial {json.dumps(updated_data)}, response: {response}")
             except Exception as e:
-                logger.error(f"Error processing file {file}: {e}")
+                logger.error(f"Error processing file {file_name}: {e}")
                 continue
-    # Call run_matchengine once after all files processed, if any were successful
-    if any_success:
-        system.run_matchengine()
+        else:
+            logger.error(f"File not found: {full_path}")
+            continue
+    
+    return any_success
+
+def _process_trials_to_update(trials_to_update, any_success):
+    """
+    Process trials that need to be updated.
+    
+    Parameters:
+    trials_to_update (list): List of tuples containing (file_name, matchminer_id, protocol_id, protocol_no, etag)
+    any_success (bool): Current success status
+    
+    Returns:
+    bool: Updated success status
+    """
+    for trial_to_update in trials_to_update: 
+        file_name = trial_to_update[0]
+        matchminer_id = trial_to_update[1]
+        protocol_id = trial_to_update[2]
+        protocol_no = trial_to_update[3]
+        etag = trial_to_update[4]
+
+        full_path = os.path.join(config.TRIAL_DIR, file_name)
+        if os.path.isfile(full_path):
+            try:
+                with open(full_path, 'r', encoding='utf-8') as json_file:
+                    data = json.load(json_file)
+                    data['protocol_id'] = protocol_id
+                    data['protocol_no'] = protocol_no
+
+                    response = put_trial(matchminer_id, data, etag)
+                    if response and response.status_code >= 200 and response.status_code < 300:
+                        logger.info(f"Successfully updated {file_name}")
+                        
+                        # Move processed file to processed_folder with retry logic
+                        # dest_path = os.path.join(processed_folder, file_name)
+                        # _move_file_with_retry(full_path, dest_path)
+                        any_success = True
+                    else:
+                        logger.error(f"Error while updating trial {json.dumps(data)}, response: {response}")
+            except Exception as e:
+                logger.error(f"Error processing file {file_name}: {e}")
+                continue
+        else:
+            logger.error(f"File not found: {full_path}")
+            continue
+    
+    return any_success
+
+def _process_trials_to_close(trials_to_close, any_success):
+    """
+    Process trials that need to be closed.
+    
+    Parameters:
+    trials_to_close (list): List of tuples containing (matchminer_id, trial_data_in_mm)
+    any_success (bool): Current success status
+    
+    Returns:
+    bool: Updated success status
+    """
+    for trial_to_close in trials_to_close:
+        matchminer_id = trial_to_close[0]
+        trial_data_in_mm = trial_to_close[1]
+        response = close_trial(matchminer_id, trial_data_in_mm, force_refresh_matchengine=True)
+        if response:
+            any_success = True
+            logger.info(f"Successfully closed {matchminer_id}")
+        else:
+            logger.error(f"Error while closing trial {matchminer_id}")
     
     return any_success
 
@@ -346,6 +507,13 @@ def save_to_file(data: dict, file_name :str, format:str):
         with open(path_to_save_at, "w") as json_file: 
             json.dump(data, json_file)
 
+def _get_trial_file_name(trial_to_process: dict):
+    if trial_to_process['nct_id'] != "NA": 
+        file_name = trial_to_process['nct_id'] + '.json'
+    else:
+        file_name = trial_to_process['local_protocol_ids'].split('|')[0] + '.json' #assuming  that the file name is same as first local protocol id
+    return file_name
+
 def get_trial_by_protocol_no(protocol_no: str):
     """
     Fetch a trial from matchminer system by protocol_no via GET request.
@@ -376,6 +544,116 @@ def get_trial_by_protocol_no(protocol_no: str):
             return items[0]  # Return the first matching trial
         else:
             print(f"No trial found with protocol_no: {protocol_no}")
+            return None
+    except requests.exceptions.HTTPError as err:
+        print(f"HTTP error occurred: {err}, {err.response.content}")
+    except Exception as err:
+        print(f"Other error occurred: {err}")
+    return None
+
+def get_trial_by_mm_id(_id: str):
+    """
+    Fetch a trial from matchminer system by matchminer id via GET request.
+
+    Parameters:
+    _id (str): Matchminer id to search for
+
+    Returns:
+    dict or None: Trial data if found, else None
+    """
+    try:
+        endpoint_url = f'{urllib.parse.urljoin(f"{config.MATCHMINER_SERVER}", config.TRIAL_ENDPOINT)}'
+        headers = {
+            'Authorization': f"Basic {config.TOKEN}",
+            'Content-Type': "application/json"
+        }
+        # Build filter for protocol_no
+        params = {
+            'where': json.dumps({"_id": _id})
+        }
+        response = requests.get(endpoint_url, headers=headers, params=params, verify=False)
+        response.raise_for_status()
+        data = response.json()
+        items = data.get("_items", [])
+        if items:
+            if len(items) > 1:
+                logger.warning(f"Found {len(items)} trials with id: {_id}. Returning the first one.")
+            return items[0]  # Return the first matching trial
+        else:
+            print(f"No trial found with id: {_id}")
+            return None
+    except requests.exceptions.HTTPError as err:
+        print(f"HTTP error occurred: {err}, {err.response.content}")
+    except Exception as err:
+        print(f"Other error occurred: {err}")
+    return None
+
+def get_trial_by_nct_id(nct_id: str):
+    """
+    Fetch a trial from matchminer system by nct_id via GET request.
+
+    Parameters:
+    nct_id (str): NCT ID to search for
+
+    Returns:
+    dict or None: Trial data if found, else None
+    """
+    try:
+        endpoint_url = f'{urllib.parse.urljoin(f"{config.MATCHMINER_SERVER}", config.TRIAL_ENDPOINT)}'
+        headers = {
+            'Authorization': f"Basic {config.TOKEN}",
+            'Content-Type': "application/json"
+        }
+        # Build filter for protocol_no
+        params = {
+            'where': json.dumps({"nct_id": nct_id})
+        }
+        response = requests.get(endpoint_url, headers=headers, params=params, verify=False)
+        response.raise_for_status()
+        data = response.json()
+        items = data.get("_items", [])
+        if items:
+            if len(items) > 1:
+                logger.warning(f"Found {len(items)} trials with nct_id: {nct_id}. Returning the first one.")
+            return items[0]  # Return the first matching trial
+        else:            
+            return None
+    except requests.exceptions.HTTPError as err:
+        print(f"HTTP error occurred: {err}, {err.response.content}")
+    except Exception as err:
+        print(f"Other error occurred: {err}")
+    return None
+
+def get_trial_by_local_protocol_ids(local_protocol_ids: list):
+    """
+    Fetch a trial from matchminer system by local protocol ids via GET request.
+
+    Parameters:
+    local_protocol_ids (list): list of local protocol ids
+
+    Returns:
+    dict or None: Trial data if found, else None
+    """
+    try:
+        endpoint_url = f'{urllib.parse.urljoin(f"{config.MATCHMINER_SERVER}", config.TRIAL_ENDPOINT)}'
+        headers = {
+            'Authorization': f"Basic {config.TOKEN}",
+            'Content-Type': "application/json"
+        }
+        # Build filter for protocol_no
+        params = {
+            'where': json.dumps({"protocol_ids":{"$in":[local_protocol_ids.join(',')]}})
+        }
+        response = requests.get(endpoint_url, headers=headers, params=params, verify=False)
+        response.raise_for_status()
+        data = response.json()
+        items = data.get("_items", [])
+        if items:
+            if len(items) > 1:
+                logger.warning(f"Found {len(items)} trials with nct_id: {local_protocol_ids}. Returning the first one.")
+            return items[0]  # Return the first matching trial
+        else:
+            print(f"No trial found with local_protocol_ids: {local_protocol_ids}")
             return None
     except requests.exceptions.HTTPError as err:
         print(f"HTTP error occurred: {err}, {err.response.content}")
@@ -443,6 +721,49 @@ def update_trial_by_protocol_no(protocol_no: str, updated_json_file_name :str):
         response.raise_for_status()
         # Only run matchengine for single update, not in batch
         system.run_matchengine()
+        return True
+    except requests.exceptions.HTTPError as err:
+        print(f"HTTP error occurred: {err}, {err.response.content}")
+    except Exception as err:
+        print(f"Other error occurred: {err}")
+    return False
+
+def close_trial(mm_id: str, existing_trial :dict, force_refresh_matchengine: bool = False):
+    """
+    Close a trial in matchminer system by matchminer id via PUT request.
+
+    Parameters:
+    mm_id (str): Matchminer id to search for
+    existing_trial (dict): Existing trial data
+
+    Returns:
+    bool: True if update was successful, False otherwise
+    """
+    
+    id = mm_id
+    etag = existing_trial.get('_etag')
+    existing_trial['status'] = 'closed'    
+
+    existing_trial.pop('_id', None)   
+    existing_trial.pop('_etag', None)
+    existing_trial.pop('_summary', None)    
+    existing_trial.pop('_updated', None)
+    existing_trial.pop('_created', None)
+    existing_trial.pop('_links', None)
+
+    try:
+        endpoint_url = f'{urllib.parse.urljoin(f"{config.MATCHMINER_SERVER}", config.TRIAL_ENDPOINT)}/{id}'
+        print(f"Closing trial at {endpoint_url}")
+        headers = {
+            'Authorization': f"Basic {config.TOKEN}",
+            'Content-Type': "application/json",
+            'If-Match': etag  
+        }
+        response = requests.put(endpoint_url, headers=headers, json=existing_trial, verify=False)
+        response.raise_for_status()
+
+        if force_refresh_matchengine:
+            system.run_matchengine()
         return True
     except requests.exceptions.HTTPError as err:
         print(f"HTTP error occurred: {err}, {err.response.content}")
